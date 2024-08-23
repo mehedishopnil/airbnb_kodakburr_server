c81d7f54a533fe470b1c7f4495a6c456acc18ec6
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-08-23 20:12:20 +0600

    final update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json()); // Middleware to parse JSON in requests
 
 
-const uri = `mongodb+srv://${process.env.DB_USERs}:${process.env.DB_PASS}@cluster0.g0ozh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0ozh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
